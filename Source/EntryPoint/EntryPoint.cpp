d4d869d1b58770e42ad619486928fef1cf0a1b2e
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-04-16 13:01:49 -0700

    Update EntryPoint.cpp

--- Source/EntryPoint/EntryPoint.cpp
+++ Source/EntryPoint/EntryPoint.cpp
@@ -2,18 +2,7 @@
 using namespace YuBCore;
 
 int main() {
-
-    LaunchRobloxGame("17574618959");
-
-    WaitForRobloxWindow();
-
-    DWORD pid = YuBCore::GetProcessIdByName(L"RobloxPlayerBeta.exe");
-
-    //YuBCore::SuspendThreads(pid);
-
-    if (!pid || !YuBCore::attach(pid, "RobloxPlayerBeta.exe")) return 1;
-
     YuBCore::dump();
-
-    CloseRoblox();
 }
+
+
