c6550cd1f8c264be82832783b12ea226962fa48f
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-04-17 20:53:32 +0300

    Update EntryPoint.cpp

--- Source/EntryPoint/EntryPoint.cpp
+++ Source/EntryPoint/EntryPoint.cpp
@@ -2,7 +2,9 @@
 using namespace YuBCore;
 
 int main() {
+	YuBCore::LaunchRobloxGame("17574618959");
+	YuBCore::WaitForRobloxWindow();
+	YuBCore::msgbox("YuB-X Dumper", "Byfron Who?");
     YuBCore::dump();
 }
-
 
