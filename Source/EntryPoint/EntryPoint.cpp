cfadaa6201ca551a9a044ef17acacafef3140a8a
BaconWareMain <164041817+BaconWareMain@users.noreply.github.com>
2025-04-18 11:30:21 -0700

    Update EntryPoint.cpp
    timer added #

--- Source/EntryPoint/EntryPoint.cpp
+++ Source/EntryPoint/EntryPoint.cpp
@@ -1,10 +1,20 @@
+#include <iostream>
+#include <chrono>
+
 #include "../../YuBCore.hpp"
+
+
+
 using namespace YuBCore;
 
 int main() {
-	YuBCore::LaunchRobloxGame("17574618959");
-	YuBCore::WaitForRobloxWindow();
-	YuBCore::msgbox("YuB-X Dumper", "Byfron Who?");
+	//YuBCore::LaunchRobloxGame("17574618959");
+	//YuBCore::WaitForRobloxWindow();
+	//YuBCore::msgbox("YuB-X Dumper", "Byfron Who?");
+    auto start = std::chrono::high_resolution_clock::now();
     YuBCore::dump();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "[*] dump() completed in " << duration.count() << " seconds." << std::endl;
 }
 
