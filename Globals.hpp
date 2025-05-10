66f9cddb7f16896fffe141c6bcac7233c2502b3e
BaconWareMain <164041817+BaconWareMain@users.noreply.github.com>
2025-05-10 04:09:50 -0700

    Update Globals.hpp

--- Globals.hpp
+++ Globals.hpp
@@ -1,6 +1,6 @@
 namespace Globals {
     extern bool XrefDebug;
     extern bool PatternDebug;
-    extern bool StartGameBeforeDummp;
+    extern bool StartGameBeforeDump;
     extern bool CloseGameAfterDump;
 }
